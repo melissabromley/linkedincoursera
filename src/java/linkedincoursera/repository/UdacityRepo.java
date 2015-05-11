c4b4059d5fbe92e204788272089c636b91c509ea
mbromley56@gmail.com <mbromley56@gmail.com>
2015-05-10 23:19:56 -0700

    Udacity courses

---
+++ src/java/linkedincoursera/repository/UdacityRepo.java
@@ -0,0 +1,22 @@
+package linkedincoursera.repository;
+
+import linkedincoursera.model.UdacityCourse;
+import linkedincoursera.util.DBConnection;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+/**
+ * Created by melissa on 5/10/15.
+ */
+@Component
+public class UdacityRepo {
+    static MongoTemplate mongoTemplate = DBConnection.getConnection();
+
+    public void addCourses(List<UdacityCourse> courses) {
+        for(UdacityCourse course : courses) {
+            mongoTemplate.save(course, "udacityCourses");
+        }
+    }
+}
