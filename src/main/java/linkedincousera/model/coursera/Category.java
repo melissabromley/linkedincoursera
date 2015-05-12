2cbababfb28bb9d8a032ef5a094cb910f13654da
swethabobba92 <swethabobba03@gmail.com>
2015-05-12 15:09:43 -0700

    category

---
+++ src/main/java/linkedincousera/model/coursera/Category.java
@@ -0,0 +1,18 @@
+package linkedincoursera.model.coursera;
+
+import java.util.List;
+
+/**
+ * Created by harsh on 5/9/15.
+ */
+public class Category {
+    List<Integer> categories;
+
+    public List<Integer> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(List<Integer> categories) {
+        this.categories = categories;
+    }
+}
